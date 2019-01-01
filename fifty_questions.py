a46cfcf9da9a8fdb6e54c9fa0d074a506a2dc2cf
Brian Hicks <brianhicks@Brians-Air.attlocal.net>
2018-12-31 16:56:37 -0800

    converted tabs to spaces

--- fifty_questions.py
+++ fifty_questions.py
@@ -5,217 +5,217 @@ import cv2 as cv
 
 class FiftyQuestionTest:
 
-	def __init__(self, page):
-		self.page = page
-		self.answers = []
-		self.unsure = []
-		self.images = []
-		self.version = None
-		self.id = ""
-		self.answersOffset = None
+   def __init__(self, page):
+      self.page = page
+      self.answers = []
+      self.unsure = []
+      self.images = []
+      self.version = None
+      self.id = ""
+      self.answersOffset = None
 
-	def getAnswers(self):
-		return self.answers
+   def getAnswers(self):
+      return self.answers
 
-	def getUnsure(self):
-		return self.unsure
+   def getUnsure(self):
+      return self.unsure
 
-	def getImages(self):
-		return self.images
+   def getImages(self):
+      return self.images
 
-	def getVersion(self):
-		return self.version
+   def getVersion(self):
+      return self.version
 
-	def getId(self):
-		return self.id
+   def getId(self):
+      return self.id
 
-	# check if answer contour is within the correct coordinates
-	@staticmethod
-	def answerInBounds(x, y):
-		if 120 <= y <= 1800:
-			# within left column bounds
-			if 180 <= x <= 500:
-				return True
-			# within right column bounds
-			elif 770 <= x <= 1090:
-				return True
-		return False 
+   # check if answer contour is within the correct coordinates
+   @staticmethod
+   def answerInBounds(x, y):
+      if 120 <= y <= 1800:
+         # within left column bounds
+         if 180 <= x <= 500:
+            return True
+         # within right column bounds
+         elif 770 <= x <= 1090:
+            return True
+      return False 
 
-	# check if version contour is within the correct coordinates
-	@staticmethod
-	def versionInBounds(x, y):
-		if 760 <= x <= 1080 and 310 <= y <= 320:
-			return True
-		else:
-			return False
+   # check if version contour is within the correct coordinates
+   @staticmethod
+   def versionInBounds(x, y):
+      if 760 <= x <= 1080 and 310 <= y <= 320:
+         return True
+      else:
+         return False
 
-	# check if id contour is within the correct coordinates
-	@staticmethod
-	def idInBounds(x, y):
-		if 15 <= x <= 760 and 15 <= y <= 600:
-			return True
-		else:
-			return False
+   # check if id contour is within the correct coordinates
+   @staticmethod
+   def idInBounds(x, y):
+      if 15 <= x <= 760 and 15 <= y <= 600:
+         return True
+      else:
+         return False
 
-  	# crop image slice for undetermined questions
-	def getImageSlice(self, questionNum, minY, maxY, offset):
-		diff = int((maxY - minY) / 25)
+   # crop image slice for undetermined questions
+   def getImageSlice(self, questionNum, minY, maxY, offset):
+      diff = int((maxY - minY) / 25)
 
-		if 1 <= questionNum <= 25:
-			return self.page[(offset + minY + diff * (questionNum - 1)) : (offset + minY + diff * questionNum), 150 : 650]
-		elif 26 <= questionNum <= 50:
-			return self.page[(offset + minY + diff * (questionNum - 26)) : (offset + minY + diff * (questionNum - 25)), 675 : 1175]
-		else:
-			return None
+      if 1 <= questionNum <= 25:
+         return self.page[(offset + minY + diff * (questionNum - 1)) : (offset + minY + diff * questionNum), 150 : 650]
+      elif 26 <= questionNum <= 50:
+         return self.page[(offset + minY + diff * (questionNum - 26)) : (offset + minY + diff * (questionNum - 25)), 675 : 1175]
+      else:
+         return None
 
-	# return contour for answers box in test
-	def getAnswersContour(self):
-		# threshold the page and find boxes within the page
-		_, threshold = cv.threshold(self.page, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
-		_, contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-		contours = sorted(contours, key=cv.contourArea, reverse=True)
+   # return contour for answers box in test
+   def getAnswersContour(self):
+      # threshold the page and find boxes within the page
+      _, threshold = cv.threshold(self.page, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+      _, contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+      contours = sorted(contours, key=cv.contourArea, reverse=True)
 
-		(_, self.answersOffset, _, _) = cv.boundingRect(contours[2])
-		peri = cv.arcLength(contours[3], True)
-		approx = cv.approxPolyDP(contours[3], 0.02 * peri, True)
-		return four_point_transform(threshold, approx.reshape(4, 2))
+      (_, self.answersOffset, _, _) = cv.boundingRect(contours[2])
+      peri = cv.arcLength(contours[3], True)
+      approx = cv.approxPolyDP(contours[3], 0.02 * peri, True)
+      return four_point_transform(threshold, approx.reshape(4, 2))
 
-	# return contour for version box in test
-	def getVersionContour(self):
-		# threshold the page and find boxes within the page
-		_, threshold = cv.threshold(self.page, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
-		_, contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-		contours = sorted(contours, key=cv.contourArea, reverse=True)
+   # return contour for version box in test
+   def getVersionContour(self):
+      # threshold the page and find boxes within the page
+      _, threshold = cv.threshold(self.page, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+      _, contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+      contours = sorted(contours, key=cv.contourArea, reverse=True)
 
-		peri = cv.arcLength(contours[6], True)
-		approx = cv.approxPolyDP(contours[6], 0.02 * peri, True)
-		return four_point_transform(threshold, approx.reshape(4, 2))
+      peri = cv.arcLength(contours[6], True)
+      approx = cv.approxPolyDP(contours[6], 0.02 * peri, True)
+      return four_point_transform(threshold, approx.reshape(4, 2))
 
-	# return contour for id contour in test
-	def getIdContour(self):
-		# threshold the page and find boxes within the page
-		_, threshold = cv.threshold(self.page, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
-		_, contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-		contours = sorted(contours, key=cv.contourArea, reverse=True)
+   # return contour for id contour in test
+   def getIdContour(self):
+      # threshold the page and find boxes within the page
+      _, threshold = cv.threshold(self.page, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+      _, contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+      contours = sorted(contours, key=cv.contourArea, reverse=True)
 
-		peri = cv.arcLength(contours[5], True)
-		approx = cv.approxPolyDP(contours[5], 0.02 * peri, True)
-		return four_point_transform(threshold, approx.reshape(4, 2))
+      peri = cv.arcLength(contours[5], True)
+      approx = cv.approxPolyDP(contours[5], 0.02 * peri, True)
+      return four_point_transform(threshold, approx.reshape(4, 2))
 
-	@staticmethod
-	def gradeAnswersColumn(self, column, columnNum, answersContour, minY, maxY):
-		# each field has 5 bubbles so loop in batches of 5
-		for (question, i) in enumerate(np.arange(0, len(column), 5)):
-			contours, _ = cutils.sort_contours(column[i:i + 5])
-			bubbled = ""
+   @staticmethod
+   def gradeAnswersColumn(self, column, columnNum, answersContour, minY, maxY):
+      # each field has 5 bubbles so loop in batches of 5
+      for (question, i) in enumerate(np.arange(0, len(column), 5)):
+         contours, _ = cutils.sort_contours(column[i:i + 5])
+         bubbled = ""
 
-			for (j, c) in enumerate(contours):
-				mask = np.zeros(answersContour.shape, dtype="uint8")
-				cv.drawContours(mask, [c], -1, 255, -1)
-				mask = cv.bitwise_and(answersContour, answersContour, mask=mask)
-				total = cv.countNonZero(mask)
+         for (j, c) in enumerate(contours):
+            mask = np.zeros(answersContour.shape, dtype="uint8")
+            cv.drawContours(mask, [c], -1, 255, -1)
+            mask = cv.bitwise_and(answersContour, answersContour, mask=mask)
+            total = cv.countNonZero(mask)
 
-				# if ~50% bubbled, count as marked
-				if total > 1700:
-					bubbled += chr(j + 65)
-				# count as unsure	
-				elif total > 1000:
-					bubbled = '?'
-					self.unsure.append(question + 1 + (25 * columnNum))
-					self.images.append(getImageSlice(question + 1, minY, maxY, self.answersOffset))
-					break
+            # if ~50% bubbled, count as marked
+            if total > 1700:
+               bubbled += chr(j + 65)
+            # count as unsure 
+            elif total > 1000:
+               bubbled = '?'
+               self.unsure.append(question + 1 + (25 * columnNum))
+               self.images.append(getImageSlice(question + 1, minY, maxY, self.answersOffset))
+               break
 
-			self.answers.append(bubbled)
+         self.answers.append(bubbled)
 
-	def gradeAnswers(self, answersContour):
-		# find bubbles in question box
-		_, allContours, _ = cv.findContours(answersContour, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-		answerContours = []
-		yValues = []
+   def gradeAnswers(self, answersContour):
+      # find bubbles in question box
+      _, allContours, _ = cv.findContours(answersContour, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+      answerContours = []
+      yValues = []
 
-		for contour in allContours:
-			(x, y, w, h) = cv.boundingRect(contour)
+      for contour in allContours:
+         (x, y, w, h) = cv.boundingRect(contour)
 
-			if w >= 45 and h >= 45 and self.answerInBounds(x, y):
-				answerContours.append(contour)
-				tup = (y, h)
-				yValues.append(tup)
+         if w >= 45 and h >= 45 and self.answerInBounds(x, y):
+            answerContours.append(contour)
+            tup = (y, h)
+            yValues.append(tup)
 
-		minY = yValues[len(yValues) - 1][0]
-		maxY = yValues[0][0] + yValues[0][1]
+      minY = yValues[len(yValues) - 1][0]
+      maxY = yValues[0][0] + yValues[0][1]
 
-		# grade bubbles in question box
-		answerContours, _ = cutils.sort_contours(answerContours, method="left-to-right")
-		length = len(answerContours)
-		mid = int(len(answerContours) / 2)
-		column1 = answerContours[0 : mid]
-		column2 = answerContours[mid : length]
+      # grade bubbles in question box
+      answerContours, _ = cutils.sort_contours(answerContours, method="left-to-right")
+      length = len(answerContours)
+      mid = int(len(answerContours) / 2)
+      column1 = answerContours[0 : mid]
+      column2 = answerContours[mid : length]
 
-		# grade questions 1-25
-		column1, _ = cutils.sort_contours(column1, method="top-to-bottom")
-		self.gradeAnswersColumn(self, column1, 0, answersContour, minY, maxY)
+      # grade questions 1-25
+      column1, _ = cutils.sort_contours(column1, method="top-to-bottom")
+      self.gradeAnswersColumn(self, column1, 0, answersContour, minY, maxY)
 
-		# grade questions 26-50
-		column2, _ = cutils.sort_contours(column2, method="top-to-bottom")
-		self.gradeAnswersColumn(self, column2, 1, answersContour, minY, maxY)
+      # grade questions 26-50
+      column2, _ = cutils.sort_contours(column2, method="top-to-bottom")
+      self.gradeAnswersColumn(self, column2, 1, answersContour, minY, maxY)
 
 #TODO version in bounds
-	def gradeVersion(self, versionContour):
-		# find bubbles in version box
-		_, allContours, _ = cv.findContours(versionContour, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-		versionContours = []
+   def gradeVersion(self, versionContour):
+      # find bubbles in version box
+      _, allContours, _ = cv.findContours(versionContour, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+      versionContours = []
 
-		for contour in allContours:
-			(x, y, w, h) = cv.boundingRect(contour)
-			aspectRatio = w / float(h)
+      for contour in allContours:
+         (x, y, w, h) = cv.boundingRect(contour)
+         aspectRatio = w / float(h)
 
-			if w >= 45 and h >= 45 and self.versionInBounds(x, y):
-				versionContours.append(contour)
+         if w >= 45 and h >= 45 and self.versionInBounds(x, y):
+            versionContours.append(contour)
 
-		# grade bubbles in version box
-		versionContours, _ = cutils.sort_contours(versionContours, method="left-to-right")
-		maxCount = None
+      # grade bubbles in version box
+      versionContours, _ = cutils.sort_contours(versionContours, method="left-to-right")
+      maxCount = None
 
-		for (j, c) in enumerate(versionContours):
-			mask = np.zeros(versionContour.shape, dtype="uint8")
-			cv.drawContours(mask, [c], -1, 255, -1)
-			mask = cv.bitwise_and(versionContour, versionContour, mask=mask)
-			total = cv.countNonZero(mask)
+      for (j, c) in enumerate(versionContours):
+         mask = np.zeros(versionContour.shape, dtype="uint8")
+         cv.drawContours(mask, [c], -1, 255, -1)
+         mask = cv.bitwise_and(versionContour, versionContour, mask=mask)
+         total = cv.countNonZero(mask)
 
-			if self.version is None or total > maxCount:
-				self.version = chr(j + 65)
-				maxCount = total
+         if self.version is None or total > maxCount:
+            self.version = chr(j + 65)
+            maxCount = total
 
 #TODO id in bounds
-	def gradeId(self, idContour):
-		# find bubbles in id box
-		_, allContours, _ = cv.findContours(idContour, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-		idContours = []
+   def gradeId(self, idContour):
+      # find bubbles in id box
+      _, allContours, _ = cv.findContours(idContour, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+      idContours = []
 
-		for contour in allContours:
-			(x, y, w, h) = cv.boundingRect(contour)
-			aspectRatio = w / float(h)
+      for contour in allContours:
+         (x, y, w, h) = cv.boundingRect(contour)
+         aspectRatio = w / float(h)
 
-			if w >= 45 and h >= 45 and self.idInBounds(x, y):
-				idContours.append(contour)
+         if w >= 45 and h >= 45 and self.idInBounds(x, y):
+            idContours.append(contour)
 
-		# grade bubbles in id box
-		idContours, _ = cutils.sort_contours(idContours, method="left-to-right")
+      # grade bubbles in id box
+      idContours, _ = cutils.sort_contours(idContours, method="left-to-right")
 
-		# each field has 10 possibilities so loop in batches of 10
-		for (q, i) in enumerate(np.arange(0, len(idContours), 10)):
-			contours, _ = cutils.sort_contours(idContours[i:i + 10], method="top-to-bottom")
-			bubbled = None
-			maxCount = None
+      # each field has 10 possibilities so loop in batches of 10
+      for (q, i) in enumerate(np.arange(0, len(idContours), 10)):
+         contours, _ = cutils.sort_contours(idContours[i:i + 10], method="top-to-bottom")
+         bubbled = None
+         maxCount = None
 
-			for (j, c) in enumerate(contours):
-				mask = np.zeros(idContour.shape, dtype="uint8")
-				cv.drawContours(mask, [c], -1, 255, -1)
-				mask = cv.bitwise_and(idContour, idContour, mask=mask)
-				total = cv.countNonZero(mask)
+         for (j, c) in enumerate(contours):
+            mask = np.zeros(idContour.shape, dtype="uint8")
+            cv.drawContours(mask, [c], -1, 255, -1)
+            mask = cv.bitwise_and(idContour, idContour, mask=mask)
+            total = cv.countNonZero(mask)
 
-				if bubbled is None or total > maxCount:
-					bubbled = j
-					maxCount = total
+            if bubbled is None or total > maxCount:
+               bubbled = j
+               maxCount = total
 
-			self.id += str(bubbled)
+         self.id += str(bubbled)
--- grader.py
+++ grader.py
@@ -7,86 +7,86 @@ import json
 import base64
 
 def findPage(im):
-	# convert image to grayscale then blur to better detect contours
-	imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-	blurred = cv.GaussianBlur(imgray.copy(), (5, 5), 0)
-	edged = cv.Canny(blurred, 75, 200)
+   # convert image to grayscale then blur to better detect contours
+   imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+   blurred = cv.GaussianBlur(imgray.copy(), (5, 5), 0)
+   edged = cv.Canny(blurred, 75, 200)
 
-	# find contour for entire page
-	_, contours, _ = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-	contours = sorted(contours, key=cv.contourArea, reverse=True)
-	page = None
+   # find contour for entire page
+   _, contours, _ = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+   contours = sorted(contours, key=cv.contourArea, reverse=True)
+   page = None
 
-	if len(contours) > 0:
-		# approximate the contour
-		for contour in contours:
-			peri = cv.arcLength(contour, True)
-			approx = cv.approxPolyDP(contour, 0.02 * peri, True)
+   if len(contours) > 0:
+      # approximate the contour
+      for contour in contours:
+         peri = cv.arcLength(contour, True)
+         approx = cv.approxPolyDP(contour, 0.02 * peri, True)
 
-			# verify that contour has four corners
-			if len(approx) == 4:
-				page = approx
-				break	
-	else:
-		print('No page found in image')
-		exit(0)
+         # verify that contour has four corners
+         if len(approx) == 4:
+            page = approx
+            break 
+   else:
+      print('No page found in image')
+      exit(0)
 
-	# apply perspective transform to get top down view of page
-	return four_point_transform(imgray, page.reshape(4, 2))
+   # apply perspective transform to get top down view of page
+   return four_point_transform(imgray, page.reshape(4, 2))
 
 def main():
-	# parse the arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--image", required=True, help="path to the input image")
-	args = vars(ap.parse_args())
+   # parse the arguments
+   ap = argparse.ArgumentParser()
+   ap.add_argument("-i", "--image", required=True, help="path to the input image")
+   args = vars(ap.parse_args())
 
-	# load image, convert to grayscale, blur, 
-	im = cv.imread(args["image"])
-	if im is None:
-		print('Could not find the image:', args["image"])
-		exit(0)
+   # load image, convert to grayscale, blur, 
+   im = cv.imread(args["image"])
+   if im is None:
+      print('Could not find the image:', args["image"])
+      exit(0)
 
-	# for debugging
-	#cv.namedWindow(args["image"], cv.WINDOW_NORMAL)
-	#cv.resizeWindow(args["image"], 850, 1100)
+   # for debugging
+   #cv.namedWindow(args["image"], cv.WINDOW_NORMAL)
+   #cv.resizeWindow(args["image"], 850, 1100)
 
-	page = findPage(im)
-	test = fifty_questions.FiftyQuestionTest(page)
-	#test = short_answer.ShortAnswerTest(page)
-	answersContour = test.getAnswersContour()
-	versionContour = test.getVersionContour()
-	idContour = test.getIdContour()
+   page = findPage(im)
+   test = fifty_questions.FiftyQuestionTest(page)
+   #test = short_answer.ShortAnswerTest(page)
+   answersContour = test.getAnswersContour()
+   versionContour = test.getVersionContour()
+   idContour = test.getIdContour()
 
-	test.gradeAnswers(answersContour)
-	test.gradeVersion(versionContour)
-	test.gradeId(idContour)
+   test.gradeAnswers(answersContour)
+   test.gradeVersion(versionContour)
+   test.gradeId(idContour)
 
-	answers = test.getAnswers()
-	unsure = test.getUnsure()
-	images = test.getImages()
-	version = test.getVersion()
-	studentId = test.getId()
+   answers = test.getAnswers()
+   unsure = test.getUnsure()
+   images = test.getImages()
+   version = test.getVersion()
+   studentId = test.getId()
 
-	# encode image slices into base64
-	encodedImages = []
-	for image in images:
-		_, binary = cv.imencode('.png', image)
-		encoded = base64.b64encode(binary)
-		encodedImages.append(encoded.decode("utf-8"))
+   # encode image slices into base64
+   encodedImages = []
+   for image in images:
+      _, binary = cv.imencode('.png', image)
+      encoded = base64.b64encode(binary)
+      encodedImages.append(encoded.decode("utf-8"))
 
-	data = {"Student ID" : studentId, "Version" : version, "Answers" : answers, "Unsure" : unsure, "Images" : encodedImages}
-	jsonData = json.dumps(data)
+   data = {"Student ID" : studentId, "Version" : version, "Answers" : answers, "Unsure" : unsure, "Images" : encodedImages}
+   jsonData = json.dumps(data)
 
-	# for debugging
-	#for image in images:
-	#	cv.imshow("img", image)
-	#	cv.waitKey()
+   # for debugging
+   #for image in images:
+   #  cv.imshow("img", image)
+   #  cv.waitKey()
 
-	print("answers", answers)
-	print("unsure", unsure)
-	print("version", version)
-	print("id", studentId)
+   print("answers", answers)
+   print("unsure", unsure)
+   print("version", version)
+   print("id", studentId)   
 
-	return jsonData
+   return jsonData
 
-main()	
+main()   
